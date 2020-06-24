0c6c51b7f5fded273e82d11da844d51f89c34bef
Ganesh Maurya <48507920+GaneshMaurya@users.noreply.github.com>
2020-06-24 21:55:43 +0530

    Add files via upload

---
+++ CSS/Navbar Transition/transition.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {scrollFunction()};
+    
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        document.getElementById("navbar").style.height = "54px";
+        document.getElementById("logo").style.fontSize = "20px";
+    }
+    else {
+        document.getElementById("navbar").style.height = "70px";
+        document.getElementById("logo").style.fontSize = "30px";
+    }
+}
